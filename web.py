b0011acf41796ca8684f6796f43d9c1738b6f944
Aaron Swartz <me@aaronsw.com>
2011-12-20 23:24:00 -0500

    add support for URLs in addition to just text blobs

--- web.py
+++ web.py
@@ -1,14 +1,24 @@
-import os
+import os, urllib
+from feedparser import _getCharacterEncoding
+import chardet
 import html2text
 from flask import Flask, request, make_response
 
-html2text.BODY_WIDTH = 140
+# html2text.BODY_WIDTH = 140
 
 app = Flask(__name__)
+app.config['DEBUG'] = True
+
+def text_response(output):
+    response = make_response(output, 200)
+    response.mimetype = 'text/plain'
+    return response
 
 @app.route("/")
 def get():
-    return """
+    url = request.args.get('url')
+    if not url:
+        return """
 <!doctype html>
 <html>
   <head>
@@ -16,27 +26,37 @@ def get():
   </head>
   <body>
     <p>This a RESTful web service that converts HTML to
-      <a href="http://daringfireball.net/projects/markdown/">Markdown</a>-like syntax using
-      <a href="http://www.aaronsw.com">Aaron Schwartz's</a> <a href="https://github.com/aaronsw/html2text">html2text.py</a>.
-      The code is available <a href="http://github.com/cv/html2text-service">on GitHub</a>.
+      <a href="http://daringfireball.net/projects/markdown/">Markdown</a>-compatible text using
+      <a href="http://www.aaronsw.com/">Aaron Swartz's</a> <a href="http://www.aaronsw.com/2002/html2text/">html2text.py</a>.</p>
     </p>
-    <p>
-      Try it out!
-    </p>
+    <form action="/" method="get">
+      <p>URL: <input type="text" name="url" /> <button type="submit">Go</button></p>
+    </form>
+      
     <form action="/" method="post">
-      <input type="submit"/>
-      <br/>
-      <textarea name="html" rows="40" cols="80"></textarea>
+      <p>Or just paste in some HTML:</p>
+      <textarea name="html" rows="20" cols="80"></textarea>
+      <p><button type="submit">Go</button>
     </form>
   </body>
 </html>
 """
+    else:
+        req = urllib.urlopen(url)
+        text = req.read()
+        encoding = _getCharacterEncoding(req.headers, text)[0]
+        if encoding == 'us-ascii': encoding = 'utf-8'
+        try:
+            text = text.decode(encoding)
+        except UnicodeDecodeError:
+            text = text.decode(chardet.detect(text)['encoding'])
+        output = html2text.html2text(text, url)
+        return text_response(output)
 
 @app.route("/", methods=['POST'])
 def post():
-    response = make_response(html2text.html2text(request.form['html']), 200)
-    response.mimetype = 'text/plain'
-    return response
+    output = html2text.html2text(request.form['html'])
+    return text_response(output)
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
